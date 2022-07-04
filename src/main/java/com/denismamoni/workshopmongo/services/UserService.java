2b1ee282b5b1a9f83260a34194fc867a3e859096
DenisWm <denismamoni@hotmail.com>
2022-07-04 08:37:25 -0400

    Deleção de usuário com delete

--- src/main/java/com/denismamoni/workshopmongo/services/UserService.java
+++ src/main/java/com/denismamoni/workshopmongo/services/UserService.java
@@ -16,22 +16,28 @@ public class UserService {
 
 	@Autowired
 	UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		List<User> users = repo.findAll();
 		return users;
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User user) {
 		return repo.insert(user);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDto(UserDTO userDto) {
 		return new User(userDto.getId(), userDto.getNome(), userDto.getEmail());
 	}
+
 }
